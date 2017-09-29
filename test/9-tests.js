26837d10ca763abf859ca365109d438b46c3b2f1
glyif <122@holbertonschool.com>
2017-09-28 17:57:16 -0700

    updated task 9 test to make request instead of static number

--- test/9-tests.js
+++ test/9-tests.js
@@ -32,8 +32,10 @@ describe('========= Checks Assignment 9 ========', function() {
         }, selector)
         .end()
         .then((windSpeed) => {
-
-          assert.equal(windSpeed, 22);
+        request ('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Francisco%2C%20CA%22)&format=json',
+          function(err, response, body) {
+            assert.equal(windSpeed, JSON.parse(body.query.results.channel.wind.speed));
+          });
           done();
         })
         .catch(done)
